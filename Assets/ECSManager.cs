93e8b07b37177e48087fbccff9783fbb5190aeab
Tkrain42 <mnemoth42@gmail.com>
2020-04-10 09:34:07 -0700

    After Sheep section

---
+++ Assets/ECSManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Random = Unity.Mathematics.Random;
+
+public class ECSManager : MonoBehaviour
+{
+    EntityManager manager;
+    public GameObject sheepPrefab;
+    public int numSheep = 15000;
+    private void Start()
+    {
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(sheepPrefab, settings);
+
+        for (int i = 0; i < numSheep; i++)
+        {
+            var instance = manager.Instantiate(prefab);
+            float3 position = transform.TransformPoint(new float3(UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(0,100), UnityEngine.Random.Range(-50, 50)));
+            float2 bubble = new float2(UnityEngine.Random.Range(0.5f, 2), 0);
+            manager.SetComponentData(instance, new Translation { Value = position });
+            manager.SetComponentData(instance, new Rotation { Value = new quaternion(0, 0, 0, 0) });
+            
+            
+        }
+    }
+}
