f4550ba91799f28dde22ca228e998cc6efc7bc8a
Tkrain42 <mnemoth42@gmail.com>
2020-04-10 11:23:30 -0700

    Through chapter 18, IcomponentData

---
+++ Assets/LargeCubeSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+public class LargeCubeSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+                        .WithName("MoveSystem")
+                        .ForEach((ref Translation position, ref Rotation rotation, ref LargeCubeData cubeData) =>
+                                 {
+                                     position.Value -= 0.01f * math.up();
+                                 })
+                        .Schedule(inputDeps);
+        return jobHandle;
+    }
+}
---
+++ Assets/MoveSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+                        .WithName("MoveSystem")
+                        .ForEach((ref Translation position, ref Rotation rotation, ref SheepData sheepData) =>
+                                 {
+                                     position.Value += 0.01f * math.up() * sheepData.speed;
+                                 })
+                        .Schedule(inputDeps);
+        return jobHandle;
+    }
+}
